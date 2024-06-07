9bcae91ae203928620aac1c365e8de85416df5ca
Fran Bejarano <86791106+FranBejarano@users.noreply.github.com>
2024-06-07 10:47:04 +0100

    - creates CRUD tests

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -1,6 +1,17 @@
 public class Question {
 
-//    int id;
+    int id;
     String question;
     String answer;
+
+    public Question(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public Question(int id, String question, String answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
 }
--- src/test/java/QuestionFetcherTest.java
+++ src/test/java/QuestionFetcherTest.java
@@ -1,46 +1,142 @@
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class QuestionFetcherTest {
+
+    private static int existingQuestions;
 
     @BeforeAll
     public static void setup(){
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = 3000;
-    }
 
-    @Test
-    public void testFetchQuestions(){
-        Response response =
-                given()
+        Response response = given()
                 .when()
-                    .get("/questions")
-                .then()
-                    .statusCode(200)
-                    .extract()
-                    .response();
+                .get("/questions");
 
-        // Basic validation,
-        response.then().body("[0].id", notNullValue());
-        response.then().body("[0].question", notNullValue());
-        response.then().body("[0].answer", notNullValue());
+        existingQuestions = response.then().extract().path("size()");
+    }
+
+    @Order(1)
+    @Test
+    public void testGetAllQuestions(){
+
+        try {
+            given()
+            .when()
+                .get("/questions")
+            .then()
+                .statusCode(200)
+            .and()
+                .assertThat()
+                .body("size()", equalTo(existingQuestions));
+
+            System.out.println("testGetAllQuestions passed");
+        } catch (AssertionError e){
+            System.out.println("Test failed: " +e.getMessage());
+            fail("testGetAllQuestions failed");
+        }
+
+    }
+
+    @Order(2)
+    @Test
+    public void testGetQuestionById(){
+        try {
+            given()
+                .pathParam("id", 1)
+            .when()
+                .get("/questions/{id}")
+            .then()
+                .statusCode(200)
+            .and()
+                .assertThat()
+                    .body("id", equalTo("1"))
+                    .body("question", equalTo("What is the capital of France?"))
+                    .body("answer", equalTo("Paris"));
+            System.out.println("testGetQuestionById passed");
+        } catch (AssertionError e){
+            System.out.println("Test failed: " +e.getMessage());
+            fail("testGetQuestionById failed");
+        }
+    }
+
+    @Order(3)
+    @Test
+    public void testCreateQuestion(){
+
+        Question question = new Question(existingQuestions+1, "What is the capital of Italy?", "Rome");
+
+        try {
+            given()
+                .contentType(ContentType.JSON)
+                .body(question)
+            .when()
+                .post("/questions")
+            .then()
+                .statusCode(201)
+            .and()
+                .assertThat()
+                .body("answer", equalTo("Rome"));
+
+            System.out.println("testCreateQuestion passed");
+        } catch (AssertionError e){
+            System.out.println("Test failed: " +e.getMessage());
+            fail("testGetAllQuestions failed");
+        }
     }
 
     @Test
-    public void testFirstQuestion(){
-        given()
-        .when()
-            .get("/questions")
-        .then()
-            .statusCode(200)
-            .body("[0].id", equalTo("1"))
-            .body("[0].question", equalTo("What is the capital of France?"))
-            .body("[0].answer", equalTo("Paris"));
+    @Order(4)
+    public void testUpdateQuestion(){
+        Question updatedQuestion = new Question("What is the capital of Spain?", "Madrid");
+
+        try {
+            given()
+                .contentType(ContentType.JSON)
+                .body(updatedQuestion)
+                .pathParam("id", 1)
+            .when()
+                .put("/questions/{id}")
+            .then()
+                .statusCode(200)
+            .and()
+                .body("answer", equalTo("Madrid"));
+
+            System.out.println("testUpdateQuestion passed");
+        } catch (AssertionError e){
+            System.out.println("Test failed: " +e.getMessage());
+            fail("testUpdateQuestion failed");
+        }
+
+
+    }
+
+    @Test
+    @Order(5)
+    public void testDeleteQuestions(){
+        int lastQuestionId = existingQuestions;
+
+        try {
+            given()
+                .pathParam("id", lastQuestionId)
+            .when()
+                .delete("/questions/{id}")
+            .then()
+                .assertThat()
+                .statusCode(200);
+
+            System.out.println("testDeleteQuestion passed");
+        } catch (AssertionError e){
+            System.out.println("Test failed: " +e.getMessage());
+            fail("testDeleteQuestion failed");
+        }
     }
 }
